17423e3d03f7c8ce91104d37400ce6deca1bccde
Tushar-W <twankhede95@gmail.com>
2020-04-05 23:50:19 +0530

    ADDED:function for checking options i.e NO_play,LADDER,Snake

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -1,12 +1,43 @@
 #!/bin/bash -x
 
 echo "Welcome To Snake And Ladder Problem"
-startPosition=0
+#CONSTANTS
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+#VARIABLE
+playerPosition=0
 
 #roll the die
 function rollingDie() {
 	result=$((RANDOM%6 + 1))
+	echo $result
+}
+
+#cheking player options
+function checkOptions() {
+	option=$((RANDOM%3))
+	case $option in
+		$NO_Play)
+			playerPosition=$playerPosition
+			;;
+		$LADDER)
+			playerPosition=$(( $playerPosition + $( rollingDie ) ))
+			;;
+		$SNAKE)
+			if [ $playerPosition -ne 0 ];
+			then
+				playerPosition=$(( $playerPosition - $( rollingDie ) ))
+			else
+				playerPosition=0
+			fi
+			;;
+	esac
 }
 
 rollingDie
 echo $result
+#start Playing first player
+echo "Player Position:" $playerPosition
+checkOptions
+echo "Player Position:"$playerPosition
